99fb53e9ce39440da2fa8acdd515c4f23feb98d6
Urstupidsammy <sgalban4@gmail.com>
2022-12-03 06:40:03 -0800

    last push for real this time

--- src/page-template.js
+++ src/page-template.js
@@ -9,9 +9,9 @@ const generateTeam = (team) => {
                     </div>
                     <div class="card-body">
                         <ul class="list-group">
-                            <li class="list-group-item">${manager.getId()}</li>
+                            <li class="list-group-item">ID: ${manager.getId()}</li>
                             <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                            <li class="list-group-item">${manager.getOfficeNumber()}</li>
+                            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
                         </ul>
                     </div>
                 </div>
@@ -27,9 +27,9 @@ const generateTeam = (team) => {
                     </div>
                     <div class="card-body">
                         <ul class="list-group">
-                            <li class="list-group-item">${engineer.getId()}</li>
+                            <li class="list-group-item">ID: ${engineer.getId()}</li>
                             <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                            <li class="list-group-item"><a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
                         </ul>
                     </div>
                 </div>
@@ -45,7 +45,7 @@ const generateTeam = (team) => {
                     </div>
                     <div class="card-body">
                         <ul class="list-group">
-                            <li class="list-group-item">${intern.getId()}</li>
+                            <li class="list-group-item">ID: ${intern.getId()}</li>
                             <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
                             <li class="list-group-item">School: ${intern.getSchool()}</li>
                         </ul>
